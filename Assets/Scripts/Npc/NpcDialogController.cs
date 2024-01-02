c5688ed15027055ff76eca9b9a98f90bf1738363
takacsoliver <takacs.oliver2000@gmail.com>
2024-01-02 23:46:26 +0100

    - Created npc interaction controller and logic - Created npc dialog controller and logic so the player and the npc will stup during dialog

---
+++ Assets/Scripts/Npc/NpcDialogController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NpcDialogController : MonoBehaviour
+{
+    private GameObject dialogBubble;
+    private NpcMovementController movementController;
+    public int secondsPenalty = 10;
+    public List<string> dialogs = new List<string> { };
+    private System.Random random = new System.Random();
+
+    private void Start()
+    {
+        movementController = this.gameObject.GetComponent<NpcMovementController>();
+        dialogBubble = this.transform.Find("DialogBubble").gameObject;
+        dialogBubble.SetActive(false);
+    }
+
+    private void Update()
+    {
+        Vector3 newScale = dialogBubble.transform.localScale;
+
+        if (this.transform.localScale.x < 0)
+        {
+            newScale.x = 1;
+            dialogBubble.transform.localScale = newScale;
+        } else
+        {
+            newScale.x = -1;
+            dialogBubble.transform.localScale = newScale;
+        }
+    }
+
+    public void ShowNpcDialog()
+    {
+        int dialogsCount = dialogs.Count;
+        int randomDialogIndex = random.Next(0, dialogsCount);
+        string randomDialog = dialogs[randomDialogIndex];
+        dialogBubble.SetActive(true);
+        TextMeshPro dialogText = dialogBubble.transform.Find("DialogText").GetComponent<TextMeshPro>();
+        dialogText.text = randomDialog;
+
+        movementController.increaseStopTimer();
+    }
+
+    public void HideNpcDialog()
+    {
+        dialogBubble.SetActive(false);
+        this.GetComponent<Animator>().enabled = true;
+    }
+}
