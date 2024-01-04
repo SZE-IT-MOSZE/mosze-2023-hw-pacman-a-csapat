266fd3065f5eed1814466933255d1a098a351f77
KibaVitya <kovalterviktor@gmail.com>
2024-01-04 03:34:09 +0100

    Handling score and collectables spawning
    - Added a NonDestroying ScoreManager for saving values for final score
    - Level Controller handles the Scene (current level) pickupables and timer
    - Spawning Collectable Prefabs from Lists with number of types
    - Saving TimeRemained on every screen and TotalTime by adding LevelTime from every scene
    - Increasing Total Fruits Number in ScoreManager by using LevelController
    - Increasing Total Books number in ScoreManager by using LevelController
    - Updating Current Scene numbers on UI On Player Pickup

--- Assets/Scripts/Level/LevelController.cs
+++ Assets/Scripts/Level/LevelController.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
     public int maxNpcSpawnPerType = 4;
+    public int maxCollectableFruits = 2;
+    public int maxCollectableBooks = 2;
+    private int collectedFruitCurrentLevel = 0;
+    private int collectedBookCurrentLevel = 0;
+    public TextMeshProUGUI fruitText;
+    public TextMeshProUGUI bookText;
     public List<GameObject> npcPrefabs = new List<GameObject> { };
+    public List<GameObject> fruitPrefabs = new List<GameObject> { };
+    public List<GameObject> bookPrefabs = new List<GameObject> { };
 
-    // Start is called before the first frame update
+
     void Start()
     {
         InstantiateNpcPrefabs();
+        InstantiateCollectablePrefabs();
+        InstantiateCollectableNumbers();
     }
 
     private void InstantiateNpcPrefabs()
@@ -24,9 +35,47 @@ public class LevelController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InstantiateCollectablePrefabs()
     {
+        for (int i = 0; i < maxCollectableFruits; i++)
+        {
+            Instantiate(fruitPrefabs[Random.Range(0, fruitPrefabs.Count)]);
+        }
+
+        for (int i = 0; i < maxCollectableBooks; i++)
+        {
+            Instantiate(bookPrefabs[Random.Range(0, bookPrefabs.Count)]);
+        }
+    }
+
+    private void InstantiateCollectableNumbers() {
+        SetCollectedFruitOnUI(0);
+        SetCollectedBookOnUI(0);
+        ScoreManager.instance.IncreaseTotalPickupableFruits(maxCollectableFruits);
+        ScoreManager.instance.IncreaseTotalPickupableBooks(maxCollectableBooks);
+    }
+
+    private void SetCollectedFruitOnUI(int num) {
+        fruitText.text = num + "/" + maxCollectableFruits;
+    }
+
+    private void SetCollectedBookOnUI(int num) {
+        bookText.text = num + "/" + maxCollectableBooks;
+    }
+
+    public void AddCollectedFruit() {
+        collectedFruitCurrentLevel++;
+        SetCollectedFruitOnUI(collectedFruitCurrentLevel);
+        ScoreManager.instance.AddCollectedTotalFruit();
+    }
+
+    public void AddCollectedBook() {
+        collectedBookCurrentLevel++;
+        SetCollectedBookOnUI(collectedBookCurrentLevel);
+        ScoreManager.instance.AddCollectedTotalBook();
+    }
+
+    void Update() {
         
     }
 }
